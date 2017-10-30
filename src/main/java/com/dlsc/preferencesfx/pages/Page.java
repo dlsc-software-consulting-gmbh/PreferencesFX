1333f9ebeedd9f2a64b542221d1e304a8d93c796
martinfrancois <fracoisma@hotmail.com>
2017-10-30 10:01:19 +0100

    added formsfx as dependency

--- src/main/java/com/dlsc/preferencesfx/pages/Page.java
+++ src/main/java/com/dlsc/preferencesfx/pages/Page.java
@@ -1,14 +1,17 @@
 package com.dlsc.preferencesfx.pages;
 
+import com.dlsc.formsfx.model.structure.Form;
 import com.dlsc.preferencesfx.Setting;
 import javafx.scene.layout.StackPane;
 
 public class Page extends StackPane {
 
   Setting[] settings;
+  Form form;
 
   Page(Setting[] settings) {
     this.settings = settings;
+
   }
 
 }
