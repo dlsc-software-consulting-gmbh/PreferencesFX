dc238fa18930bf723372f0811942460235734e8c
martinfrancois <fracoisma@hotmail.com>
2017-10-30 10:16:19 +0100

    added demo to see if it is possible to add elements afterwards to formsfx

--- src/main/java/com/dlsc/preferencesfx/RootPane.java
+++ src/main/java/com/dlsc/preferencesfx/RootPane.java
@@ -1,5 +1,6 @@
 package com.dlsc.preferencesfx;
 
+import com.dlsc.preferencesfx.pages.Page;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.StackPane;
 
@@ -7,23 +8,29 @@ import javafx.scene.layout.StackPane;
  * Created by François Martin on 30.10.17.
  */
 public class RootPane extends StackPane {
-  Slider slider = new Slider();
 
-  PreferencesFX preferencesFX = PreferencesFX.of(
-      Category.of("Bildschirm",
-          Setting.of("Nachtmodus", Type.BOOLEAN),
-          Setting.of("Skalierung", Type.INTEGER),
-          Setting.of("Custom", slider, slider.valueProperty())
-      ),
-      Category.of("Benachrichtigungen")
-          .withSubCategories(
-              Category.of("Allgemein"),
-              Category.of("Farbe"),
-              Category.of("Schrift")
-          ),
-      Category.of("Netzbetrieb")
-          .withSubCategories(
-              Category.of("Allgemein")
-          )
-  );
+  RootPane(){
+    Slider slider = new Slider();
+
+    PreferencesFX preferencesFX = PreferencesFX.of(
+        Category.of("Bildschirm",
+            Setting.of("Nachtmodus", Type.BOOLEAN),
+            Setting.of("Skalierung", Type.INTEGER),
+            Setting.of("Custom", slider, slider.valueProperty())
+        ),
+        Category.of("Benachrichtigungen")
+            .withSubCategories(
+                Category.of("Allgemein"),
+                Category.of("Farbe"),
+                Category.of("Schrift")
+            ),
+        Category.of("Netzbetrieb")
+            .withSubCategories(
+                Category.of("Allgemein")
+            )
+    );
+
+    getChildren().add(new Page(null));
+  }
+
 }
--- src/main/java/com/dlsc/preferencesfx/pages/Page.java
+++ src/main/java/com/dlsc/preferencesfx/pages/Page.java
@@ -1,7 +1,13 @@
 package com.dlsc.preferencesfx.pages;
 
+import com.dlsc.formsfx.model.structure.Field;
 import com.dlsc.formsfx.model.structure.Form;
+import com.dlsc.formsfx.model.structure.Group;
+import com.dlsc.formsfx.view.renderer.FormRenderer;
+import com.dlsc.formsfx.view.util.ColSpan;
 import com.dlsc.preferencesfx.Setting;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.scene.layout.StackPane;
 
 public class Page extends StackPane {
@@ -9,9 +15,23 @@ public class Page extends StackPane {
   Setting[] settings;
   Form form;
 
-  Page(Setting[] settings) {
+  public Page(Setting[] settings) {
     this.settings = settings;
+    initForm();
+    getChildren().add(new FormRenderer(form));
+  }
 
+  void initForm() {
+    StringProperty s = new SimpleStringProperty("Test");
+    form = Form.of(
+        Group.of()
+    ).title("form_label");
+    form.getGroups().get(0).getFields().add(
+        Field.ofStringType(s)
+            .label("currency_label")
+            .placeholder("currency_placeholder")
+            .span(ColSpan.HALF)
+    );
   }
 
 }
