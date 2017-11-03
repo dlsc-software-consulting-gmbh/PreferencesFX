1ecf611810c0ccc621554dc28640165cf1a180c3
martinfrancois <fracoisma@hotmail.com>
2017-11-03 18:19:39 +0100

    made categorypane simpler included widget into settings

--- src/main/java/com/dlsc/preferencesfx/Setting.java
+++ src/main/java/com/dlsc/preferencesfx/Setting.java
@@ -1,43 +1,65 @@
 package com.dlsc.preferencesfx;
 
 import com.dlsc.formsfx.model.structure.Field;
-import com.dlsc.preferencesfx.widgets.Widget;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.Property;
 import javafx.beans.property.StringProperty;
 
-public class Setting<W extends Widget> {
+public class Setting<F extends Field, P extends Property> {
   private String description;
-  private W widget;
+  private Type type;
+  private F field;
+  private P value;
 
-  Setting(String description, W widget) {
+  Setting(String description, Type type, F field, P value) {
     this.description = description;
-    this.widget = widget;
+    this.field = field;
+    this.value = value;
   }
 
-  public static <W extends Widget> Setting of(String description, Type type, Property property) { //alle W sind unterschiedlich
+  public static <F extends Field, P extends Property> Setting<F, P> of(String description, Type type, P property) { //alle W sind unterschiedlich
     switch (type) {
       case BOOLEAN:
-        return new Setting(description, new Widget(Field.ofBooleanType((BooleanProperty) property), property));
+        return new Setting(
+            description,
+            type,
+            Field.ofBooleanType((BooleanProperty) property).label(description),
+            property);
       case INTEGER:
-        return new Setting(description, new Widget(Field.ofIntegerType((IntegerProperty) property), property));
+        return new Setting(
+            description,
+            type,
+            Field.ofIntegerType((IntegerProperty) property).label(description),
+            property);
       case STRING:
-        return new Setting(description, new Widget(Field.ofStringType((StringProperty) property), property));
+        return new Setting(
+            description,
+            type,
+            Field.ofStringType((StringProperty) property).label(description),
+            property);
       default:
         return null;
     }
   }
 
-  /*public static <W extends Widget> Setting of(String description, Node widget, Property property) {
-    return new Setting(description, new Widget(widget, property));
+  /*public static <F extends Field, P extends Property> Setting<F, P> of(String description, Node customControl, P value) {
+    return new Setting(description, new Widget(widget, value));
   }*/
 
   public String getDescription() {
     return description;
   }
 
-  public W getWidget() {
-    return widget;
+  public Type getType() {
+    return type;
+  }
+
+  public P valueProperty() {
+    return value;
+  }
+
+  public F getField() {
+    return field;
   }
 }
--- src/main/java/com/dlsc/preferencesfx/pages/CategoryPane.java
+++ src/main/java/com/dlsc/preferencesfx/pages/CategoryPane.java
@@ -1,16 +1,10 @@
 package com.dlsc.preferencesfx.pages;
 
-import com.dlsc.formsfx.model.structure.Field;
 import com.dlsc.formsfx.model.structure.Form;
 import com.dlsc.formsfx.model.structure.Group;
 import com.dlsc.formsfx.model.util.BindingMode;
 import com.dlsc.formsfx.view.renderer.FormRenderer;
 import com.dlsc.preferencesfx.Setting;
-import com.google.common.base.Splitter;
-import com.google.common.collect.Iterables;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.StringProperty;
 import javafx.scene.layout.StackPane;
 
 public class CategoryPane extends StackPane {
@@ -29,32 +23,9 @@ public class CategoryPane extends StackPane {
         Group.of()
     ).title("form_label");
     for (Setting setting : settings) {
-      String propertyString = setting.getWidget().valueProperty().getClass().toString();
-      propertyString = Iterables.getLast(Splitter.on(".")
-          .omitEmptyStrings()
-          .trimResults()
-          .splitToList(propertyString));
-
-      Field field = null;
-
-      switch (propertyString) {
-        case "SimpleBooleanProperty":
-          field = Field.ofBooleanType((BooleanProperty) setting.getWidget().valueProperty()).label(setting.getDescription());
-          break;
-        case "SimpleStringProperty":
-          field = Field.ofStringType((StringProperty) setting.getWidget().valueProperty()).label(setting.getDescription());
-          break;
-        case "SimpleIntegerProperty":
-          field = Field.ofIntegerType((IntegerProperty) setting.getWidget().valueProperty()).label(setting.getDescription());
-          break;
-      }
-
-      form.getGroups().get(0).getFields().add(field);
-
+      form.getGroups().get(0).getFields().add(setting.getField());
     }
-
     form.binding(BindingMode.CONTINUOUS);
-
   }
 
 }
