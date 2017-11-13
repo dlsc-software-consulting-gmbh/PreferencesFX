0ced7fab251f00829e6e74a95cfd8c556c062c7c
Marco Sanfratello <marco.sanfratello@students.fhnw.ch>
2017-11-13 09:49:48 +0100

    Fixed following issues: - Made TreeItem private - Collapsed initial all TreeItems - Replaced BorderPane with SplitPane

--- src/main/java/com/dlsc/preferencesfx/CategoryTree.java
+++ src/main/java/com/dlsc/preferencesfx/CategoryTree.java
@@ -1,44 +1,42 @@
 package com.dlsc.preferencesfx;
 
-import java.util.List;
-import java.util.Objects;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 
+import java.util.List;
+import java.util.Objects;
+
 public class CategoryTree extends TreeView {
 
-  private List<Category> categories;
-  TreeItem<Category> rootItem;
+    private List<Category> categories;
+    private TreeItem<Category> rootItem;
 
-  public CategoryTree(List<Category> categories) {
-    this.categories = categories;
-    setupParts();
-    layoutParts();
-  }
-
-  private void setupParts() {
-    rootItem = new TreeItem();
-    addRecursive(rootItem, categories);
-  }
-
-  private void addRecursive(TreeItem treeItem, List<Category> categories) {
-    for (Category category : categories) {
-      TreeItem<Category> item = new TreeItem<>(category);
-      item.setExpanded(true);
-      if (!Objects.equals(category.getChildren(), null)) {
-        addRecursive(item, category.getChildren());
-      }
-      treeItem.getChildren().add(item);
+    public CategoryTree(List<Category> categories) {
+        this.categories = categories;
+        setupParts();
+        layoutParts();
     }
-  }
 
-  private void layoutParts() {
-    this.setRoot(rootItem);
-    this.setShowRoot(false);
-    this.getRoot().setExpanded(true);
-    this.getSelectionModel().select(0);
-  }
+    private void setupParts() {
+        rootItem = new TreeItem();
+        addRecursive(rootItem, categories);
+    }
+
+    private void addRecursive(TreeItem treeItem, List<Category> categories) {
+        for (Category category : categories) {
+            TreeItem<Category> item = new TreeItem<>(category);
+            if (!Objects.equals(category.getChildren(), null)) {
+                addRecursive(item, category.getChildren());
+            }
+            treeItem.getChildren().add(item);
+        }
+    }
+
+    private void layoutParts() {
+        this.setRoot(rootItem);
+        this.setShowRoot(false);
+        this.getRoot().setExpanded(true);
+        this.getSelectionModel().select(0);
+    }
 
 }
--- src/main/java/com/dlsc/preferencesfx/PreferencesFX.java
+++ src/main/java/com/dlsc/preferencesfx/PreferencesFX.java
@@ -1,42 +1,50 @@
 package com.dlsc.preferencesfx;
 
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TreeItem;
+
 import java.util.Arrays;
 import java.util.List;
-import javafx.scene.control.TreeItem;
-import javafx.scene.layout.BorderPane;
 
-public class PreferencesFX extends BorderPane {
+public class PreferencesFX extends SplitPane {
 
-  private List<Category> categories;
-  private CategoryTree categoryTree;
+    private List<Category> categories;
+    private CategoryTree categoryTree;
 
-  PreferencesFX(Category[] categories) {
-    this.categories = Arrays.asList(categories);
-    setupParts();
-    layoutParts();
-    setupListeners();
-  }
+    PreferencesFX(Category[] categories) {
+        this.categories = Arrays.asList(categories);
+        setupParts();
+        layoutParts();
+        setupListeners();
+    }
 
-  private void setupParts() {
-    categoryTree = new CategoryTree(categories);
-  }
+    private void setupParts() {
+        categoryTree = new CategoryTree(categories);
+    }
 
-  private void layoutParts() {
-    setLeft(categoryTree);
-    setCenter(this.categories.get(0).getPage());
-  }
+    private void layoutParts() {
+        getItems().addAll(
+                categoryTree,
+                this.categories.get(0).getPage()
+        );
 
-  private void setupListeners() {
-    categoryTree.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-      setCenter(((Category) ((TreeItem) newValue).getValue()).getPage());
-    });
-  }
+        this.setDividerPositions(0.2f);
+    }
 
-  public static PreferencesFX of(Category... categories) {
-    return new PreferencesFX(categories);
-  }
+    private void setupListeners() {
+        categoryTree.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            double dividerPosition = this.getDividerPositions()[0]; // Save the old divider position. When you set the new item, it resets the position.
+            getItems().remove(((Category) ((TreeItem) oldValue).getValue()).getPage()); // Removes the old page
+            getItems().add(((Category) ((TreeItem) newValue).getValue()).getPage()); // Places the new page
+            this.setDividerPositions(dividerPosition); // Sets the saved divider position
+        });
+    }
 
-  public List<Category> getCategories() {
-    return categories;
-  }
+    public static PreferencesFX of(Category... categories) {
+        return new PreferencesFX(categories);
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
 }
