1294c88f0319fb73688aef1ff4c3eb62b8b7493a
Genron <marco.sanfratello@students.fhnw.ch>
2017-11-14 09:34:44 +0100

    Initialized constants

--- src/main/java/com/dlsc/preferencesfx/PreferencesFX.java
+++ src/main/java/com/dlsc/preferencesfx/PreferencesFX.java
@@ -8,43 +8,47 @@ import java.util.List;
 
 public class PreferencesFX extends SplitPane {
 
-    private List<Category> categories;
-    private CategoryTree categoryTree;
+  public static final int FIRST_DIVIDER = 0;
+  public static final int CATEGORY_PANE = 1;
+  public static final float DIVIDER_POSITION = 0.2f;
+  public static final int INITIAL_CATEGORY = 0;
+  private List<Category> categories;
+  private CategoryTree categoryTree;
 
-    PreferencesFX(Category[] categories) {
-        this.categories = Arrays.asList(categories);
-        setupParts();
-        layoutParts();
-        setupListeners();
-    }
+  PreferencesFX(Category[] categories) {
+    this.categories = Arrays.asList(categories);
+    setupParts();
+    layoutParts();
+    setupListeners();
+  }
 
-    private void setupParts() {
-        categoryTree = new CategoryTree(categories);
-    }
+  private void setupParts() {
+    categoryTree = new CategoryTree(categories);
+  }
 
-    private void layoutParts() {
-        getItems().addAll(
-                categoryTree,
-                this.categories.get(0).getPage()
-        );
+  private void layoutParts() {
+    getItems().addAll(
+        categoryTree,
+        this.categories.get(INITIAL_CATEGORY).getPage()
+    );
 
-        this.setDividerPositions(0.2f);
-    }
+    this.setDividerPositions(DIVIDER_POSITION);
+  }
 
-    private void setupListeners() {
-        categoryTree.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            double dividerPosition = this.getDividerPositions()[0]; // Save the old divider position. When you set the new item, it resets the position.
-            getItems().remove(((Category) ((TreeItem) oldValue).getValue()).getPage()); // Removes the old page
-            getItems().add(((Category) ((TreeItem) newValue).getValue()).getPage()); // Places the new page
-            this.setDividerPositions(dividerPosition); // Sets the saved divider position
-        });
-    }
+  private void setupListeners() {
+    categoryTree.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+      double dividerPosition = this.getDividerPositions()[FIRST_DIVIDER]; // Save the old divider position. When you set the new item, it resets the position.
+      getItems().remove(CATEGORY_PANE); // Removes the old page
+      getItems().add(((Category) ((TreeItem) newValue).getValue()).getPage()); // Places the new page
+//            this.setDividerPositions(dividerPosition); // Sets the saved divider position
+    });
+  }
 
-    public static PreferencesFX of(Category... categories) {
-        return new PreferencesFX(categories);
-    }
+  public static PreferencesFX of(Category... categories) {
+    return new PreferencesFX(categories);
+  }
 
-    public List<Category> getCategories() {
-        return categories;
-    }
+  public List<Category> getCategories() {
+    return categories;
+  }
 }
