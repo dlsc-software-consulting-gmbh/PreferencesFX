a11af1934dd8b881527d6da51116031a1cebedd0
martinfrancois <fracoisma@hotmail.com>
2017-11-24 15:22:48 +0100

    changed extend so it's possible to choose the control

--- src/main/java/com/dlsc/preferencesfx/DoubleSliderControl.java
+++ src/main/java/com/dlsc/preferencesfx/DoubleSliderControl.java
@@ -1,13 +1,27 @@
 package com.dlsc.preferencesfx;
 
+import com.dlsc.formsfx.model.structure.BooleanField;
 import com.dlsc.formsfx.model.structure.DoubleField;
+import com.dlsc.formsfx.view.controls.SimpleControl;
 import com.dlsc.formsfx.view.controls.SimpleNumberControl;
+import javafx.scene.control.Label;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.layout.VBox;
+import org.controlsfx.control.ToggleSwitch;
 
 /**
  * Created by François Martin on 24.11.17.
  */
-public class DoubleSliderControl extends SimpleNumberControl<DoubleField, Double> {
+public class DoubleSliderControl extends SimpleControl<DoubleField> {
+  /**
+   * - fieldLabel is the container that displays the label property of the
+   * field.
+   * - toggleSwitch is the toggle switch to set user input.
+   * - container holds the toggle so that it can be styled properly.
+   */
+  private Label fieldLabel;
+  private ToggleSwitch toggleSwitch;
+  private VBox container;
 
   /**
    * {@inheritDoc}
@@ -16,8 +30,36 @@ public class DoubleSliderControl extends SimpleNumberControl<DoubleField, Double
   public void initializeParts() {
     super.initializeParts();
 
-    getStyleClass().add("double-slider-control");
-    editableSpinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(-Double.MAX_VALUE, Double.MAX_VALUE, field.getValue()));
+    getStyleClass().add("toggle-control");
+
+    fieldLabel = new Label(field.labelProperty().getValue());
+    toggleSwitch = new ToggleSwitch();
+    container = new VBox();
+    toggleSwitch.setSelected(field.getValue());
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void layoutParts() {
+    super.layoutParts();
+
+    container.getChildren().add(toggleSwitch);
+
+    add(fieldLabel, 0, 0, 2, 1);
+    add(container, 2, 0, field.getSpan() - 2, 1);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setupBindings() {
+    super.setupBindings();
+
+    toggleSwitch.disableProperty().bind(field.editableProperty().not());
+    fieldLabel.textProperty().bind(field.labelProperty());
   }
 
   /**
@@ -26,8 +68,29 @@ public class DoubleSliderControl extends SimpleNumberControl<DoubleField, Double
   @Override
   public void setupValueChangedListeners() {
     super.setupValueChangedListeners();
+    field.userInputProperty().addListener((observable, oldValue, newValue) -> {
+      toggleSwitch.setSelected(Boolean.parseBoolean(field.getUserInput()));
+    });
 
-    field.tooltipProperty().addListener((observable, oldValue, newValue) -> toggleTooltip(editableSpinner));
-    field.errorMessagesProperty().addListener((observable, oldValue, newValue) -> toggleTooltip(editableSpinner));
+    field.errorMessagesProperty().addListener(
+        (observable, oldValue, newValue) -> toggleTooltip(toggleSwitch));
+    field.tooltipProperty().addListener(
+        (observable, oldValue, newValue) -> toggleTooltip(toggleSwitch));
+
+    toggleSwitch.focusedProperty().addListener(
+        (observable, oldValue, newValue) -> toggleTooltip(toggleSwitch));
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setupEventHandlers() {
+    setOnMouseEntered(event -> toggleTooltip(toggleSwitch));
+    setOnMouseExited(event -> toggleTooltip(toggleSwitch));
+
+    toggleSwitch.selectedProperty().addListener((observable, oldValue, newValue) -> {
+      field.userInputProperty().setValue(String.valueOf(newValue));
+    });
   }
 }
