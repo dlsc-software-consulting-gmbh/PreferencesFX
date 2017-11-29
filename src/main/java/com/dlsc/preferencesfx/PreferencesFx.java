db043c179f1784aa0c3cb4fc4a44629aff52cb07
Genron <marco.sanfratello@students.fhnw.ch>
2017-11-29 14:22:36 +0100

    implementation of updating settings afterwards goes on.

--- src/main/java/com/dlsc/preferencesfx/PreferencesFx.java
+++ src/main/java/com/dlsc/preferencesfx/PreferencesFx.java
@@ -52,7 +52,7 @@ public class PreferencesFx extends MasterDetailPane {
         .filter(Objects::nonNull)     // remove all null
         .flatMap(Collection::stream)
         .collect(Collectors.toList())
-        .forEach(setting -> setting.updateFromPreferences(storageHandler));
+        .forEach(setting -> setting.updateFromPreferences("", storageHandler));
   }
 
   /**
