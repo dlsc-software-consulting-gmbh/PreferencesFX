060ca2e1b9f20ed201cedf1d71e94a0ce9b0cbc3
martinfrancois <fracoisma@hotmail.com>
2017-12-03 15:53:35 +0100

    added bindings and changed to properties

--- src/main/java/com/dlsc/preferencesfx/history/History.java
+++ src/main/java/com/dlsc/preferencesfx/history/History.java
@@ -1,17 +1,28 @@
 package com.dlsc.preferencesfx.history;
 
 import com.dlsc.preferencesfx.Setting;
-import java.util.ArrayList;
-import java.util.List;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  * Created by François Martin on 02.12.17.
  */
 public class History {
 
-  private List<Change> changes = new ArrayList<>();
-  private int position = 0;
-  private int validPosition = 0;
+  private ObservableList<Change> changes = FXCollections.observableArrayList();
+  private IntegerProperty position = new SimpleIntegerProperty(0);
+  private IntegerProperty validPosition = new SimpleIntegerProperty(0);
+  private BooleanProperty undoAvailable = new SimpleBooleanProperty(false);
+  private BooleanProperty redoAvailable = new SimpleBooleanProperty(false);
+
+  public History() {
+    undoAvailable.bind(position.greaterThan(0));
+    redoAvailable.bind(position.lessThan(validPosition));
+  }
 
   public void attachChangeListener(Setting setting) {
     setting.valueProperty().addListener((observable, oldValue, newValue) -> {
@@ -20,30 +31,75 @@ public class History {
   }
 
   public void addChange(Change change) {
-    changes.set(position, change);
+    if (changes.get(position) != null) { // if there is already an element at the current position
+      changes.set(position, change);
+    } else {
+      changes.add(position, change);
+    }
     validPosition = ++position;
+  }
+
+  public boolean undo() {
+    Change lastChange = prev();
+    if (lastChange != null) {
+      lastChange.undo();
+      return true;
+    }
+    return false;
+  }
+
+  public boolean redo() {
+    Change nextChange = next();
+    if (nextChange != null) {
+      nextChange.redo();
+      return true;
+    }
+    return false;
   }
 
   public Change next() {
     if (hasNext()) {
-      return changes.get(++position);
+      return changes.get(incrementPosition());
     }
     return null;
   }
 
   public boolean hasNext() {
-    return position < validPosition;
+    return position.get() < validPosition.get();
   }
 
   public Change prev() {
     if (hasPrev()) {
-      return changes.get(--position);
+      return changes.get(decrementPosition());
     }
     return null;
   }
 
   public boolean hasPrev() {
-    return position > 0;
+    return position.get() > 0;
   }
 
+  public int incrementPosition() {
+    int nextPosition = position.get() + 1;
+    position.setValue(nextPosition);
+    return nextPosition;
+  }
+
+  public int decrementPosition() {
+    int prevPosition = position.get() - 1;
+    position.setValue(prevPosition);
+    return prevPosition;
+  }
+
+  public int incrementValidPosition() {
+    int nextPosition = validPosition.get() + 1;
+    validPosition.setValue(nextPosition);
+    return nextPosition;
+  }
+
+  public int decrementValidPosition() {
+    int prevPosition = validPosition.get() - 1;
+    validPosition.setValue(prevPosition);
+    return prevPosition;
+  }
 }
---
+++ src/main/java/com/dlsc/preferencesfx/history/HistoryPane.java
@@ -0,0 +1,9 @@
+package com.dlsc.preferencesfx.history;
+
+import javafx.scene.layout.VBox;
+
+/**
+ * Created by François Martin on 03.12.2017.
+ */
+public class HistoryPane extends VBox {
+}
