83b7f7d33efb3ee646ed90bc7f95c3284e57ce75
martinfrancois <fracoisma@hotmail.com>
2017-12-08 16:57:20 +0100

    finished new implementation of controls

--- src/main/java/com/dlsc/preferencesfx/util/formsfx/ToggleControl.java
+++ src/main/java/com/dlsc/preferencesfx/util/formsfx/ToggleControl.java
@@ -8,7 +8,7 @@ import org.controlsfx.control.ToggleSwitch;
 /**
  * Displays a control for boolean values with a toggle from ControlsFX.
  */
-public class ToggleControl extends SimpleControl<BooleanField> {
+public class ToggleControl extends SimpleControl<BooleanField, ToggleSwitch> {
   public static final double NEGATIVE_LABEL_INSETS = -17.3;
   /**
    * - fieldLabel is the container that displays the label property of the
@@ -17,8 +17,6 @@ public class ToggleControl extends SimpleControl<BooleanField> {
    * - container holds the toggle so that it can be styled properly.
    */
   private Label fieldLabel;
-  private ToggleSwitch toggleSwitch;
-  private VBox container;
 
   /**
    * {@inheritDoc}
@@ -27,27 +25,13 @@ public class ToggleControl extends SimpleControl<BooleanField> {
   public void initializeParts() {
     super.initializeParts();
 
-    getStyleClass().add("toggle-control");
+    node.getStyleClass().add("toggle-control");
 
     fieldLabel = new Label(field.labelProperty().getValue());
-    toggleSwitch = new ToggleSwitch();
+    node = new ToggleSwitch();
     // is necessary to offset the control to the left, because we don't use the provided label
-    toggleSwitch.setTranslateX(NEGATIVE_LABEL_INSETS);
-    container = new VBox();
-    toggleSwitch.setSelected(field.getValue());
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void layoutParts() {
-    super.layoutParts();
-
-    container.getChildren().add(toggleSwitch);
-
-    add(fieldLabel, 0, 0, 2, 1);
-    add(container, 2, 0, field.getSpan() - 2, 1);
+    node.setTranslateX(NEGATIVE_LABEL_INSETS);
+    node.setSelected(field.getValue());
   }
 
   /**
@@ -57,7 +41,7 @@ public class ToggleControl extends SimpleControl<BooleanField> {
   public void setupBindings() {
     super.setupBindings();
 
-    toggleSwitch.disableProperty().bind(field.editableProperty().not());
+    node.disableProperty().bind(field.editableProperty().not());
     fieldLabel.textProperty().bind(field.labelProperty());
   }
 
@@ -68,16 +52,8 @@ public class ToggleControl extends SimpleControl<BooleanField> {
   public void setupValueChangedListeners() {
     super.setupValueChangedListeners();
     field.userInputProperty().addListener((observable, oldValue, newValue) -> {
-      toggleSwitch.setSelected(Boolean.parseBoolean(field.getUserInput()));
+      node.setSelected(Boolean.parseBoolean(field.getUserInput()));
     });
-
-    field.errorMessagesProperty().addListener(
-        (observable, oldValue, newValue) -> toggleTooltip(toggleSwitch));
-    field.tooltipProperty().addListener(
-        (observable, oldValue, newValue) -> toggleTooltip(toggleSwitch));
-
-    toggleSwitch.focusedProperty().addListener(
-        (observable, oldValue, newValue) -> toggleTooltip(toggleSwitch));
   }
 
   /**
@@ -85,10 +61,7 @@ public class ToggleControl extends SimpleControl<BooleanField> {
    */
   @Override
   public void setupEventHandlers() {
-    setOnMouseEntered(event -> toggleTooltip(toggleSwitch));
-    setOnMouseExited(event -> toggleTooltip(toggleSwitch));
-
-    toggleSwitch.selectedProperty().addListener((observable, oldValue, newValue) -> {
+    node.selectedProperty().addListener((observable, oldValue, newValue) -> {
       field.userInputProperty().setValue(String.valueOf(newValue));
     });
   }
