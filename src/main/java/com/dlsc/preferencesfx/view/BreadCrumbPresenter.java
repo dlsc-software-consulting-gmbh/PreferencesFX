6cde1821726cb88b7a9c9cafd2bbdbc07ee9aeb6
Genron <marco.sanfratello@students.fhnw.ch>
2018-01-04 16:06:34 +0100

    Implemented first version of ScrumBar. Eventhandlers and binding to tree are the next steps.

--- src/main/java/com/dlsc/preferencesfx/view/BreadCrumbPresenter.java
+++ src/main/java/com/dlsc/preferencesfx/view/BreadCrumbPresenter.java
@@ -24,24 +24,27 @@ public class BreadCrumbPresenter {
     String selectedBreadcrumb = model.getDisplayedCategory().getBreadcrumb();
     String[] stringArr = selectedBreadcrumb.split(BREADCRUMB_DELIMITER);
 
-    for (int i = 0; i < stringArr.length; ++i) {
-      String crumb = stringArr[0];
-      for (int j = 1; j <= i; ++j) {
-        crumb += BREADCRUMB_DELIMITER + stringArr[j];
-      }
-      String breadcrumb = crumb;
-      Category category = model.getFlatCategoriesLst().stream().filter(
-          cat -> cat.getBreadcrumb().equals(breadcrumb)
-      ).findFirst().get();
-      categories.add(category);
+    categories = addToCategory(stringArr[0], categories);
+    for (int i = 1; i < stringArr.length; ++i) {
+      stringArr[i] = stringArr[i - 1] + BREADCRUMB_DELIMITER + stringArr[i];
+      categories = addToCategory(stringArr[i], categories);
     }
 
     Category[] categoriesArr = new Category[categories.size()];
     for (int i = 0; i < categoriesArr.length; ++i) {
       categoriesArr[i] = categories.get(i);
     }
+
     breadCrumbView.breadcrumbsItm = BreadCrumbBar.buildTreeModel(categoriesArr);
     breadCrumbView.breadCrumbBar.setSelectedCrumb(breadCrumbView.breadcrumbsItm);
+  }
+
+  private List<Category> addToCategory(String breadcrumb, List<Category> categories) {
+    Category category = model.getFlatCategoriesLst().stream().filter(
+        cat -> cat.getBreadcrumb().equals(breadcrumb)
+    ).findFirst().get();
+    categories.add(category);
+    return categories;
   }
 
   private void setupListeners() {
