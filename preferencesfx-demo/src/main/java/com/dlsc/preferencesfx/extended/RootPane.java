3c6d7f246c287259fcc0b515949dddf13ac59ac7
martinfrancois <fracoisma@hotmail.com>
2018-01-17 16:55:23 +0100

    added categories from the previous demos

--- preferencesfx-demo/src/main/java/com/dlsc/preferencesfx/extended/RootPane.java
+++ preferencesfx-demo/src/main/java/com/dlsc/preferencesfx/extended/RootPane.java
@@ -2,6 +2,7 @@ package com.dlsc.preferencesfx.extended;
 
 import com.dlsc.formsfx.model.structure.Field;
 import com.dlsc.formsfx.model.structure.IntegerField;
+import com.dlsc.formsfx.model.validators.DoubleRangeValidator;
 import com.dlsc.preferencesfx.AppStarter;
 import com.dlsc.preferencesfx.PreferencesFx;
 import com.dlsc.preferencesfx.formsfx.view.controls.IntegerSliderControl;
@@ -133,6 +134,35 @@ public class RootPane extends StackPane {
 
   private PreferencesFx createPreferences() {
     return PreferencesFx.of(AppStarter.class,
+        Category.of("General",
+            Group.of("Greeting",
+                Setting.of("Welcome Text", welcomeText)
+            ),
+            Group.of("Display",
+                Setting.of("Brightness", brightness),
+                Setting.of("Night mode", nightMode)
+            )
+        ),
+        Category.of("Screen")
+            .subCategories(
+                Category.of("Scaling & Ordering",
+                    Group.of(
+                        Setting.of("Scaling", scaling)
+                            .validate(DoubleRangeValidator.atLeast(1, "Scaling needs to be at least 1")),
+                        Setting.of("Screen name", screenName),
+                        Setting.of("Resolution", resolutionItems, resolutionSelection),
+                        Setting.of("Orientation", orientationItems, orientationSelection)
+                    ).description("Screen Options"),
+                    Group.of(
+                        Setting.of("Font Size", fontSize, 6, 36),
+                        Setting.of("Line Spacing", lineSpacing, 0, 3, 1)
+                    )
+                )
+            ),
+        Category.of("Favorites",
+            Setting.of("Favorites", favoritesItems, favoritesSelection),
+            Setting.of("Favorite Number", customControl, customControlProperty)
+        ),
         Category.of("Appearance & Behaviour")
             .subCategories(
                 Category.of("Appearance",
