44d1262e73c570c42798514bcb0804e97d5ed6fb
martinfrancois <fracoisma@hotmail.com>
2018-01-17 22:07:45 +0100

    remove unneeded constant

--- src/main/java/com/dlsc/preferencesfx/util/Constants.java
+++ src/main/java/com/dlsc/preferencesfx/util/Constants.java
@@ -1,7 +1,5 @@
 package com.dlsc.preferencesfx.util;
 
-import java.util.prefs.Preferences;
-
 public class Constants {
   public static final String SELECTED_CATEGORY = "SELECTED_CATEGORY";
 
@@ -21,12 +19,4 @@ public class Constants {
   public static final String WINDOW_HEIGHT = "WINDOW_HEIGHT";
   public static final String WINDOW_POS_X = "WINDOW_POS_X";
   public static final String WINDOW_POS_Y = "WINDOW_POS_Y";
-
-  /**
-   * Calculates how long the breadcrumb part of the key to save for {@link Preferences} can be.
-   * Since the hashcode method returns an int, the longest number is the one with a minus, so that
-   * length is taken and is subtracted off the maximum key length of {@link Preferences}.
-   */
-  public static final int PREFERENCES_KEY_BREADCRUMB_TRIM
-      = Preferences.MAX_KEY_LENGTH-(String.valueOf(Integer.MIN_VALUE).length());
 }
