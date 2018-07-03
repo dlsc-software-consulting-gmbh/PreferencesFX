8a24a2fe07e4ddfbf1286b96c254d44dbee27cdc
kivimango <balazsviktor23@gmail.com>
2018-07-03 16:05:03 +0200

    AbstractStorageHandler renamed to StorageHandler

--- preferencesfx/src/main/java/com/dlsc/preferencesfx/util/StorageHandler.java
+++ preferencesfx/src/main/java/com/dlsc/preferencesfx/util/StorageHandler.java
@@ -13,91 +13,91 @@ import java.util.prefs.Preferences;
  * @author kivimango
  */
 
-public abstract class AbstractStorageHandler {
+public interface StorageHandler {
 
     /**
      * Stores the last selected category in TreeSearchView.
      *
      * @param breadcrumb the category path as a breadcrumb string
      */
-    public abstract void saveSelectedCategory(String breadcrumb);
+    void saveSelectedCategory(String breadcrumb);
 
     /**
      * Gets the last selected category in TreeSearchView.
      *
      * @return the breadcrumb string of the selected category. null if none is found
      */
-    public abstract String loadSelectedCategory();
+    String loadSelectedCategory();
 
     /**
      * Stores the given divider position of the MasterDetailPane.
      *
      * @param dividerPosition the divider position to be stored
      */
-    public abstract void saveDividerPosition(double dividerPosition);
+    void saveDividerPosition(double dividerPosition);
 
     /**
      * Gets the stored divider position of the MasterDetailPane.
      *
      * @return the double value of the divider position. 0.2 if none is found
      */
-    public abstract double loadDividerPosition();
+    double loadDividerPosition();
 
     /**
      * Stores the window width of the PreferencesFxDialog.
      *
      * @param windowWidth the width of the window to be stored
      */
-    public abstract void saveWindowWidth(double windowWidth);
+    void saveWindowWidth(double windowWidth);
 
     /**
      * Searches for the window width of the PreferencesFxDialog.
      *
      * @return the double value of the window width. 1000 if none is found
      */
-    public abstract double loadWindowWidth();
+    double loadWindowWidth();
 
     /**
      * Stores the window height of the PreferencesFxDialog.
      *
      * @param windowHeight the height of the window to be stored
      */
-    public abstract void saveWindowHeight(double windowHeight);
+    void saveWindowHeight(double windowHeight);
 
     /**
      * Searches for the window height of the PreferencesFxDialog.
      *
      * @return the double value of the window height. 700 if none is found
      */
-    public abstract double loadWindowHeight();
+    double loadWindowHeight();
 
     /**
      * Stores the position of the PreferencesFxDialog in horizontal orientation.
      *
      * @param windowPosX the double value of the window position in horizontal orientation
      */
-    public abstract void saveWindowPosX(double windowPosX);
+    void saveWindowPosX(double windowPosX);
 
     /**
      * Searches for the horizontal window position.
      *
      * @return the double value of the horizontal window position
      */
-    public abstract double loadWindowPosX();
+    double loadWindowPosX();
 
     /**
      * Stores the position of the PreferencesFxDialog in vertical orientation.
      *
      * @param windowPosY the double value of the window position in vertical orientation
      */
-    public abstract void saveWindowPosY(double windowPosY);
+    void saveWindowPosY(double windowPosY);
 
     /**
      * Searches for the vertical window position.
      *
      * @return the double value of the vertical window position
      */
-    public abstract double loadWindowPosY();
+    double loadWindowPosY();
 
     /**
      * Serializes a given Object and saves it to the storage using the given key.
@@ -106,7 +106,7 @@ public abstract class AbstractStorageHandler {
      * @param object     the Object which will be saved
      */
     // asciidoctor Documentation - tag::storageHandlerSave[]
-    public abstract void saveObject(String breadcrumb, Object object);
+    void saveObject(String breadcrumb, Object object);
     // asciidoctor Documentation - end::storageHandlerSave[]
 
     /**
@@ -118,7 +118,7 @@ public abstract class AbstractStorageHandler {
      * @return the deserialized Object or the default Object if nothing is found
      */
     // asciidoctor Documentation - tag::storageHandlerLoad[]
-    public abstract Object loadObject(String breadcrumb, Object defaultObject);
+    Object loadObject(String breadcrumb, Object defaultObject);
     // asciidoctor Documentation - end::storageHandlerLoad[]
 
     /**
@@ -132,17 +132,14 @@ public abstract class AbstractStorageHandler {
      *                              which will be returned if nothing is found
      * @return the deserialized ObservableList or the default ObservableList if nothing is found
      */
-    public abstract ObservableList loadObservableList(
-            String breadcrumb,
-            ObservableList defaultObservableList
-    );
+    ObservableList loadObservableList(String breadcrumb, ObservableList defaultObservableList);
 
     /**
      * Clears the storage.
      * @return true if successful, false if there was an exception.
      */
-    public abstract boolean clearPreferences();
+    boolean clearPreferences();
 
-    public abstract Preferences getPreferences();
+    Preferences getPreferences();
 
 }
