0f10410676e0840300238365b3f74cb42d0c30c7
Marco Sanfratello <marco.sanfratello@karakun.com>
2019-05-08 18:53:49 +0200

    Fixed Checkstyle Errors

--- preferencesfx/src/main/java/com/dlsc/preferencesfx/formsfx/view/controls/SimpleColorPickerControl.java
+++ preferencesfx/src/main/java/com/dlsc/preferencesfx/formsfx/view/controls/SimpleColorPickerControl.java
@@ -26,12 +26,10 @@ import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-
 import javax.annotation.Nonnull;
 
 /**
- * This class provides the base implementation for a simple control to edit
- * ColorPicker values.
+ * This class provides the base implementation for a simple control to edit ColorPicker values.
  *
  * @author Sacha Schmid
  * @author Rinesch Murugathas
@@ -41,83 +39,84 @@ import javax.annotation.Nonnull;
  */
 public class SimpleColorPickerControl extends SimpleControl<StringField, StackPane> {
 
-    /**
-     * - The colorPicker is the container that displays the node to select a color value.
-     */
-    private ColorPicker colorPicker;
-    private Color initialValue;
-    private Label fieldLabel;
+  /**
+   * - The colorPicker is the container that displays the node to select a color value.
+   */
+  private ColorPicker colorPicker;
+  private Color initialValue;
+  private Label fieldLabel;
 
-    /**
-     * Create a SimpleColorPickerControl with an initial value.
-     *
-     * @param initialValue The initial color, cannot be null.
-     */
-    public SimpleColorPickerControl(@Nonnull Color initialValue) {
-        this.initialValue = initialValue;
-    }
+  /**
+   * Create a SimpleColorPickerControl with an initial value.
+   *
+   * @param initialValue The initial color, cannot be null.
+   */
+  public SimpleColorPickerControl(@Nonnull Color initialValue) {
+    this.initialValue = initialValue;
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void initializeParts() {
-        super.initializeParts();
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void initializeParts() {
+    super.initializeParts();
 
-        node = new StackPane();
-        node.getStyleClass().add("simple-text-control");
+    node = new StackPane();
+    node.getStyleClass().add("simple-text-control");
 
-        colorPicker = new ColorPicker(initialValue);
-        colorPicker.setMaxWidth(Double.MAX_VALUE);
-        colorPicker.setOnAction(event -> {
-            if (!field.valueProperty().getValue().equals(colorPicker.getValue().toString()))
-                field.valueProperty().setValue(colorPicker.getValue().toString());
-        });
+    colorPicker = new ColorPicker(initialValue);
+    colorPicker.setMaxWidth(Double.MAX_VALUE);
+    colorPicker.setOnAction(event -> {
+      if (!field.valueProperty().getValue().equals(colorPicker.getValue().toString())) {
         field.valueProperty().setValue(colorPicker.getValue().toString());
-        fieldLabel = new Label(field.labelProperty().getValue());
-    }
+      }
+    });
+    field.valueProperty().setValue(colorPicker.getValue().toString());
+    fieldLabel = new Label(field.labelProperty().getValue());
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void layoutParts() {
-        node.getChildren().addAll(colorPicker);
-        node.setAlignment(Pos.CENTER_LEFT);
-    }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void layoutParts() {
+    node.getChildren().addAll(colorPicker);
+    node.setAlignment(Pos.CENTER_LEFT);
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setupBindings() {
-        super.setupBindings();
-    }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setupBindings() {
+    super.setupBindings();
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setupValueChangedListeners() {
-        super.setupValueChangedListeners();
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setupValueChangedListeners() {
+    super.setupValueChangedListeners();
 
-        field.valueProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.isEmpty()) {
-                Color newColor = Color.valueOf(newValue);
-                if (!colorPicker.getValue().equals(newColor)) {
-                    colorPicker.setValue(newColor);
-                }
-            }
-        });
+    field.valueProperty().addListener((observable, oldValue, newValue) -> {
+      if (!newValue.isEmpty()) {
+        Color newColor = Color.valueOf(newValue);
+        if (!colorPicker.getValue().equals(newColor)) {
+          colorPicker.setValue(newColor);
+        }
+      }
+    });
 
-        field.errorMessagesProperty().addListener(
-                (observable, oldValue, newValue) -> toggleTooltip(colorPicker)
-        );
-        field.tooltipProperty().addListener(
-                (observable, oldValue, newValue) -> toggleTooltip(colorPicker)
-        );
-        colorPicker.focusedProperty().addListener(
-                (observable, oldValue, newValue) -> toggleTooltip(colorPicker)
-        );
-    }
+    field.errorMessagesProperty().addListener(
+        (observable, oldValue, newValue) -> toggleTooltip(colorPicker)
+    );
+    field.tooltipProperty().addListener(
+        (observable, oldValue, newValue) -> toggleTooltip(colorPicker)
+    );
+    colorPicker.focusedProperty().addListener(
+        (observable, oldValue, newValue) -> toggleTooltip(colorPicker)
+    );
+  }
 }
--- preferencesfx/src/main/java/com/dlsc/preferencesfx/formsfx/view/controls/SimpleDirectoryChooserControl.java
+++ preferencesfx/src/main/java/com/dlsc/preferencesfx/formsfx/view/controls/SimpleDirectoryChooserControl.java
@@ -20,6 +20,7 @@ package com.dlsc.preferencesfx.formsfx.view.controls;
  */
 
 import com.dlsc.formsfx.model.structure.StringField;
+import java.io.File;
 import javafx.beans.binding.Bindings;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -31,12 +32,11 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Window;
-
 import javax.annotation.Nullable;
-import java.io.File;
 
 /**
- * This class provides the base implementation for a simple control to select or enter a directory path.
+ * This class provides the base implementation for a simple control to select or enter a directory
+ * path.
  *
  * @author Rinesch Murugathas
  * @author Sacha Schmid
@@ -46,148 +46,150 @@ import java.io.File;
  */
 public class SimpleDirectoryChooserControl extends SimpleControl<StringField, StackPane> {
 
-    /**
-     * - The fieldLabel is the container that displays the label property of the field.
-     * - The editableField allows users to modify the field's value.
-     * - The readOnlyLabel displays the field's value if it is not editable.
-     */
-    private TextField editableField;
-    private TextArea editableArea;
-    private Label readOnlyLabel;
-    private Label fieldLabel;
-    private Button directoryChooserButton = new Button();
-    private Window windowOwner;
-    private HBox hBox = new HBox();
-    private String buttonText;
-    private File initialDirectory;
+  /**
+   * - The fieldLabel is the container that displays the label property of the field. - The
+   * editableField allows users to modify the field's value. - The readOnlyLabel displays the
+   * field's value if it is not editable.
+   */
+  private TextField editableField;
+  private TextArea editableArea;
+  private Label readOnlyLabel;
+  private Label fieldLabel;
+  private Button directoryChooserButton = new Button();
+  private Window windowOwner;
+  private HBox contentBox = new HBox();
+  private String buttonText;
+  private File initialDirectory;
 
-    /**
-     * Create a new SimpleDirectoryChooserControl.
-     *
-     * @param windowOwner      A windowOwner that is needed to show the DirectoryChooser
-     * @param buttonText       Text for the button to show, e.g. "Browse"
-     * @param initialDirectory An optional initial path, can be null.
-     */
-    public SimpleDirectoryChooserControl(Window windowOwner, String buttonText, @Nullable File initialDirectory) {
-        this.windowOwner = windowOwner;
-        this.buttonText = buttonText;
-        this.initialDirectory = initialDirectory;
+  /**
+   * Create a new SimpleDirectoryChooserControl.
+   *
+   * @param windowOwner A windowOwner that is needed to show the DirectoryChooser
+   * @param buttonText Text for the button to show, e.g. "Browse"
+   * @param initialDirectory An optional initial path, can be null.
+   */
+  public SimpleDirectoryChooserControl(Window windowOwner, String buttonText,
+      @Nullable File initialDirectory) {
+    this.windowOwner = windowOwner;
+    this.buttonText = buttonText;
+    this.initialDirectory = initialDirectory;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void initializeParts() {
+    super.initializeParts();
+
+    node = new StackPane();
+    node.getStyleClass().add("simple-text-control");
+
+    editableField = new TextField(field.getValue());
+    editableArea = new TextArea(field.getValue());
+
+    readOnlyLabel = new Label(field.getValue());
+    fieldLabel = new Label(field.labelProperty().getValue());
+    editableField.setPromptText(field.placeholderProperty().getValue());
+
+    DirectoryChooser directoryChooser = new DirectoryChooser();
+
+    if (field.valueProperty().get().equals("null")) {
+      field.valueProperty().set("");
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void initializeParts() {
-        super.initializeParts();
+    directoryChooserButton.setOnAction(event -> {
+      if (initialDirectory != null) {
+        directoryChooser.setInitialDirectory(initialDirectory);
+      } else if (!field.valueProperty().get().trim().isEmpty()) {
+        directoryChooser.setInitialDirectory(new File(field.valueProperty().get()));
+      }
 
-        node = new StackPane();
-        node.getStyleClass().add("simple-text-control");
+      File dir = directoryChooser.showDialog(windowOwner);
+      if (dir != null) {
+        editableField.setText(dir.getAbsolutePath());
+      }
+    });
 
-        editableField = new TextField(field.getValue());
-        editableArea = new TextArea(field.getValue());
+    directoryChooserButton.setText(buttonText);
 
-        readOnlyLabel = new Label(field.getValue());
-        fieldLabel = new Label(field.labelProperty().getValue());
-        editableField.setPromptText(field.placeholderProperty().getValue());
+    StackPane fieldStackPane = new StackPane();
+    fieldStackPane.getChildren().addAll(editableField, editableArea, readOnlyLabel);
+    fieldStackPane.setAlignment(Pos.CENTER_LEFT);
+    HBox.setHgrow(fieldStackPane, Priority.ALWAYS);
 
-        DirectoryChooser directoryChooser = new DirectoryChooser();
+    contentBox.getChildren().addAll(fieldStackPane, directoryChooserButton);
+  }
 
-        if (field.valueProperty().get().equals("null"))
-            field.valueProperty().set("");
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void layoutParts() {
+    readOnlyLabel.getStyleClass().add("read-only-label");
 
-        directoryChooserButton.setOnAction(event -> {
-            if (initialDirectory != null) {
-                directoryChooser.setInitialDirectory(initialDirectory);
-            } else if (!field.valueProperty().get().trim().isEmpty()) {
-                directoryChooser.setInitialDirectory(new File(field.valueProperty().get()));
-            }
+    readOnlyLabel.setPrefHeight(26);
 
-            File dir = directoryChooser.showDialog(windowOwner);
-            if (dir != null) {
-                editableField.setText(dir.getAbsolutePath());
-            }
-        });
+    editableArea.getStyleClass().add("simple-textarea");
+    editableArea.setPrefRowCount(5);
+    editableArea.setPrefHeight(80);
+    editableArea.setWrapText(true);
 
-        directoryChooserButton.setText(buttonText);
-
-        StackPane fieldStackPane = new StackPane();
-        fieldStackPane.getChildren().addAll(editableField, editableArea, readOnlyLabel);
-        fieldStackPane.setAlignment(Pos.CENTER_LEFT);
-        HBox.setHgrow(fieldStackPane, Priority.ALWAYS);
-
-        hBox.getChildren().addAll(fieldStackPane, directoryChooserButton);
+    if (field.isMultiline()) {
+      node.setPrefHeight(80);
+      readOnlyLabel.setPrefHeight(80);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void layoutParts() {
-        readOnlyLabel.getStyleClass().add("read-only-label");
+    node.getChildren().add(contentBox);
 
-        readOnlyLabel.setPrefHeight(26);
+    node.setAlignment(Pos.CENTER_LEFT);
+  }
 
-        editableArea.getStyleClass().add("simple-textarea");
-        editableArea.setPrefRowCount(5);
-        editableArea.setPrefHeight(80);
-        editableArea.setWrapText(true);
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setupBindings() {
+    super.setupBindings();
 
-        if (field.isMultiline()) {
-            node.setPrefHeight(80);
-            readOnlyLabel.setPrefHeight(80);
-        }
+    editableArea.visibleProperty().bind(Bindings.and(field.editableProperty(),
+        field.multilineProperty()));
+    editableField.visibleProperty().bind(Bindings.and(field.editableProperty(),
+        field.multilineProperty().not()));
+    readOnlyLabel.visibleProperty().bind(field.editableProperty().not());
 
-        node.getChildren().add(hBox);
+    editableField.textProperty().bindBidirectional(field.userInputProperty());
+    editableArea.textProperty().bindBidirectional(field.userInputProperty());
+    readOnlyLabel.textProperty().bind(field.userInputProperty());
+    editableField.promptTextProperty().bind(field.placeholderProperty());
+    editableArea.promptTextProperty().bind(field.placeholderProperty());
 
-        node.setAlignment(Pos.CENTER_LEFT);
-    }
+    editableArea.managedProperty().bind(editableArea.visibleProperty());
+    editableField.managedProperty().bind(editableField.visibleProperty());
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setupBindings() {
-        super.setupBindings();
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setupValueChangedListeners() {
+    super.setupValueChangedListeners();
 
-        editableArea.visibleProperty().bind(Bindings.and(field.editableProperty(),
-                field.multilineProperty()));
-        editableField.visibleProperty().bind(Bindings.and(field.editableProperty(),
-                field.multilineProperty().not()));
-        readOnlyLabel.visibleProperty().bind(field.editableProperty().not());
+    field.multilineProperty().addListener((observable, oldValue, newValue) -> {
+      node.setPrefHeight(newValue ? 80 : 0);
+      readOnlyLabel.setPrefHeight(newValue ? 80 : 26);
+    });
 
-        editableField.textProperty().bindBidirectional(field.userInputProperty());
-        editableArea.textProperty().bindBidirectional(field.userInputProperty());
-        readOnlyLabel.textProperty().bind(field.userInputProperty());
-        editableField.promptTextProperty().bind(field.placeholderProperty());
-        editableArea.promptTextProperty().bind(field.placeholderProperty());
+    field.errorMessagesProperty().addListener((observable, oldValue, newValue) ->
+        toggleTooltip(field.isMultiline() ? editableArea : editableField)
+    );
 
-        editableArea.managedProperty().bind(editableArea.visibleProperty());
-        editableField.managedProperty().bind(editableField.visibleProperty());
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setupValueChangedListeners() {
-        super.setupValueChangedListeners();
-
-        field.multilineProperty().addListener((observable, oldValue, newValue) -> {
-            node.setPrefHeight(newValue ? 80 : 0);
-            readOnlyLabel.setPrefHeight(newValue ? 80 : 26);
-        });
-
-        field.errorMessagesProperty().addListener((observable, oldValue, newValue) ->
-                toggleTooltip(field.isMultiline() ? editableArea : editableField)
-        );
-
-        editableField.focusedProperty().addListener(
-                (observable, oldValue, newValue) -> toggleTooltip(editableField)
-        );
-        editableArea.focusedProperty().addListener(
-                (observable, oldValue, newValue) -> toggleTooltip(editableArea)
-        );
-    }
+    editableField.focusedProperty().addListener(
+        (observable, oldValue, newValue) -> toggleTooltip(editableField)
+    );
+    editableArea.focusedProperty().addListener(
+        (observable, oldValue, newValue) -> toggleTooltip(editableArea)
+    );
+  }
 
 }
