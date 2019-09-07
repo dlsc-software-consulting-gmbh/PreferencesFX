c72352a14c7a1119d2b989da08d518611227ac7d
martinfrancois <fracoisma@hotmail.com>
2019-09-07 12:40:24 +0200

    add log statements

--- preferencesfx/src/main/java/com/dlsc/preferencesfx/view/PreferencesFxDialog.java
+++ preferencesfx/src/main/java/com/dlsc/preferencesfx/view/PreferencesFxDialog.java
@@ -188,12 +188,17 @@ public class PreferencesFxDialog extends DialogPane {
     } else {
       // check if we already added an event filter to the apply button, to avoid adding it twice
       if (applyBtn != applyWithEventBtn) {
-        applyBtn.addEventFilter(ActionEvent.ACTION, event -> {
-          event.consume();
-          model.saveSettings();
-        });
+        LOGGER.trace("Adding event filter to apply button");
+        applyBtn.addEventFilter(
+            ActionEvent.ACTION,
+            event -> {
+              event.consume();
+              model.saveSettings();
+            });
 
         applyWithEventBtn = applyBtn;
+      } else {
+        LOGGER.trace("Event filter was already added previously to apply button");
       }
     }
   }
