6b9fe859c06e1630cdb6cb10f1afd7c145e5ab6b
Dirk Lemmermann <dlemmermann@gmail.com>
2021-04-26 11:33:34 +0200

    Fixed an issue in demo module related to logging.

--- preferencesfx-demo/src/main/java/module-info.java
+++ preferencesfx-demo/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.dlsc.preferencesfx.demo {
 
     requires com.dlsc.preferencesfx;
+    requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
+    requires org.apache.logging.log4j.slf4j;
 
     exports com.dlsc.preferencesfx.demo;
 
