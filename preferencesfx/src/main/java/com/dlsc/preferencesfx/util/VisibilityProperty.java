cbdb10e7d4200743c648224b63d14c344a57cd7d
Jörn Bungartz <joern@bungartz.pm>
2022-08-08 13:47:54 -0400

    Added examples for using new visibility feature into extended demo. Added documentation for VisibilityProperty helpers.

--- preferencesfx/src/main/java/com/dlsc/preferencesfx/util/VisibilityProperty.java
+++ preferencesfx/src/main/java/com/dlsc/preferencesfx/util/VisibilityProperty.java
@@ -9,6 +9,18 @@ import javafx.beans.property.SimpleBooleanProperty;
 public interface VisibilityProperty {
     BooleanProperty get();
 
+    /**
+     * Creates a {@link VisibilityProperty} that is based on the given {@link Property}.
+     *
+     * The {@link VisibilityProperty} value will be set automatically when the given {@link Property} value changes.
+     * The new value of the {@link VisibilityProperty} will be the result of the given {@link Function} applied to the
+     * new value of the {@link Property}.
+     *
+     * @param property
+     * @param visibilityFunc
+     * @return
+     * @param <T>
+     */
     static <T> VisibilityProperty of(Property<T> property, Function<T, Boolean> visibilityFunc) {
         return () -> {
             BooleanProperty visibilityProperty = new SimpleBooleanProperty(true);
@@ -18,4 +30,19 @@ public interface VisibilityProperty {
             return visibilityProperty;
         };
     }
+
+    /**
+     * Simplified constructor for {@link VisibilityProperty} that is based on the given Boolean type {@link Property}
+     *
+     * The value of the {@link VisibilityProperty} will be set to the value of the given {@link Property}, i.e.,
+     * whenever the referenced {@link Property} is true, the value of the @link VisibilityProperty} will be true as well.
+     *
+     * @param property
+     * @return
+     * @param <T>
+     */
+    static <T> VisibilityProperty of(Property<Boolean> property) {
+        return VisibilityProperty.of(property, (newValue) -> newValue);
+    }
+
 }
