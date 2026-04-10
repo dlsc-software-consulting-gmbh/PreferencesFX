cc51a4bb4eeb0fd9e0d533f12b24075f2e582944
dlemmermann <dlemmermann@gmail.com>
2026-04-10 13:52:38 +0200

    Removed unused `dialog_i18n` demo and updated Log4j dependencies to version 2.25.3.

--- preferencesfx-demo/src/main/java/module-info.java
+++ preferencesfx-demo/src/main/java/module-info.java
@@ -3,11 +3,10 @@ module com.dlsc.preferencesfx.demo {
     requires com.dlsc.preferencesfx;
     requires org.apache.logging.log4j;
     requires org.apache.logging.log4j.core;
-    requires org.apache.logging.log4j.slf4j;
+    requires org.apache.logging.log4j.slf4j.impl;
     requires com.google.gson;
 
     exports com.dlsc.preferencesfx.demo;
-    exports  com.dlsc.preferencesfx.demo.dialog_i18n;
 
     opens com.dlsc.preferencesfx.demo;
     opens com.dlsc.preferencesfx.demo.extended;
